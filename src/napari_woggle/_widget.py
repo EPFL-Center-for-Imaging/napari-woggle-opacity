21f0b9e682fc54a8f26be7f5c5c6dd90f2c7ecac
Mallory Wittwer <mallory.wittwer@epfl.ch>
2023-10-22 12:23:21 +0200

    Improve widget

--- src/napari_woggle/_widget.py
+++ src/napari_woggle/_widget.py
@@ -2,14 +2,13 @@ import time
 from napari.qt.threading import thread_worker
 from PyQt5.QtCore import Qt
 from qtpy.QtWidgets import (
-    QVBoxLayout, 
     QWidget, 
-    QHBoxLayout, 
     QLabel, 
     QComboBox, 
     QPushButton, 
-    QSlider,
-    QRadioButton,
+    QDoubleSpinBox,
+    QGridLayout,
+    QSizePolicy,
 )   
 
 class WoggleWidget(QWidget):
@@ -17,56 +16,54 @@ class WoggleWidget(QWidget):
         super().__init__()
 
         self.viewer = napari_viewer
-        self.viewer.bind_key('w', lambda k: self._toggle_start_process())
+        self.viewer.bind_key('w', lambda k: self._start())
         
         self.step = 0.05
-        self.woggle_speed = 0.05
-        self.flag = False
-        self.woggle_transition_fnct = self.smooth_transition
+        self.is_woggling = False
+
+        self.transitions = {
+            'Smooth': self.smooth_transition,
+            'Sharp': self.sharp_transition,
+        }
         
-        layout = QVBoxLayout(self)
-        layout.setAlignment(Qt.AlignTop)
-        layout.addWidget(self)
-        self.setLayout(layout)
+        # Layout
+        grid_layout = QGridLayout()
+        grid_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(grid_layout)
 
-        qw00 = QWidget(self)
-        self.layout().addWidget(qw00)
-        sub00 = QHBoxLayout()
-        qw00.setLayout(sub00)
-        sub00.addWidget(QLabel("Image", self))
+        # Image
         self.cb_image = QComboBox()
-        sub00.addWidget(self.cb_image)
+        self.cb_image.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.cb_image.currentTextChanged.connect(self._on_selected_layer_changed)
+        grid_layout.addWidget(QLabel("Image", self), 0, 0)
+        grid_layout.addWidget(self.cb_image, 0, 1)
 
-        # Woggle speed
-        qw01 = QWidget(self)
-        self.layout().addWidget(qw01)
-        sub01 = QHBoxLayout()
-        qw01.setLayout(sub01)
-        sub01.addWidget(QLabel("Speed", self))
-        self.woggle_speed_slider = QSlider(Qt.Horizontal)
-        self.woggle_speed_slider.valueChanged.connect(self.update_woggle_speed)
-        sub01.addWidget(self.woggle_speed_slider)
+        # Speed
+        self.speed_spinbox = QDoubleSpinBox()
+        self.speed_spinbox.setMinimum(1)
+        self.speed_spinbox.setMaximum(10)
+        self.speed_spinbox.setSingleStep(1)
+        self.speed_spinbox.setValue(self.speed)
+        self.speed_spinbox.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Speed", self), 1, 0)
+        grid_layout.addWidget(self.speed_spinbox, 1, 1)
 
-        # Woggle shape
-        qw02 = QWidget(self)
-        self.layout().addWidget(qw02)
-        sub02 = QHBoxLayout()
-        qw02.setLayout(sub02)
-        sub02.addWidget(QLabel("Transition", self))
-        self.b1 = QRadioButton("Smooth")
-        self.b1.setChecked(True)
-        self.b1.toggled.connect(lambda:self.update_woggle_transition_type(self.b1))
-        sub02.addWidget(self.b1)
-        self.b2 = QRadioButton("Sharp")
-        self.b2.toggled.connect(lambda:self.update_woggle_transition_type(self.b2))
-        sub02.addWidget(self.b2)
+        # Transition
+        self.cb_transition = QComboBox()
+        self.cb_transition.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.cb_transition.addItems(list(self.transitions.keys()))
+        grid_layout.addWidget(QLabel("Transition", self), 2, 0)
+        grid_layout.addWidget(self.cb_transition, 2, 1)
 
         # Start button        
-        self.start_btn = QPushButton('Start woggling', self)
-        self.start_btn.clicked.connect(self._toggle_start_process)
-        self.layout().addWidget(self.start_btn)
+        self.start_btn = QPushButton('Start', self)
+        self.start_btn.clicked.connect(self._start)
+        grid_layout.addWidget(self.start_btn, 3, 0, 1, 2)
 
-        self.viewer.layers.events.inserted.connect(self._add_rename_event)
+        # Setup layer callbacks
+        self.viewer.layers.events.inserted.connect(
+            lambda e: e.value.events.name.connect(self._on_layer_change)
+        )
         self.viewer.layers.events.inserted.connect(self._on_layer_change)
         self.viewer.layers.events.removed.connect(self._on_layer_change)
         self._on_layer_change(None)
@@ -74,12 +71,21 @@ class WoggleWidget(QWidget):
         import skimage.data
         self.viewer.add_image(skimage.data.coins())
 
-    def update_woggle_speed(self):
-        speed = self.woggle_speed_slider.value()  # Values are in [0-100]
-        self.woggle_speed = (100 - speed) / 100 * 0.10 + 0.001
+    def _on_layer_change(self, e):
+        self.cb_image.clear()
+        for x in self.viewer.layers:
+            self.cb_image.addItem(x.name, x.data)
+
+    def _on_selected_layer_changed(self, *args):
+        self.is_woggling = False
+        self.start_btn.setText('Start')
+
+    @property
+    def speed(self):
+        return 0.1 - self.speed_spinbox.value() / 100 + 0.01
 
     def smooth_transition(self):
-        time.sleep(self.woggle_speed)
+        time.sleep(self.speed)
         
         if (self.layer.opacity - 2*self.step <= 0.0) | (self.layer.opacity - 2*self.step >= 1.0):
             self.step = -self.step
@@ -89,7 +95,7 @@ class WoggleWidget(QWidget):
         return opacity
 
     def sharp_transition(self):
-        wait_time = 1.0 / abs(self.step) * self.woggle_speed
+        wait_time = 1.0 / abs(self.step) * self.speed
         time.sleep(wait_time)
 
         opacity = self.layer.opacity
@@ -103,51 +109,29 @@ class WoggleWidget(QWidget):
         
         return opacity
 
-    def update_woggle_transition_type(self, b):
-        print('Selected transition type: ', b.text())
-        if b.text() == 'Smooth':
-            self.woggle_transition_fnct = self.smooth_transition
-        elif b.text() == 'Sharp':
-            self.woggle_transition_fnct = self.sharp_transition
-        
     @thread_worker()
-    def _threaded_process(self):
-        while self.flag:
-            opacity = self.woggle_transition_fnct()
+    def _threaded_woggling(self):
+        while self.is_woggling:
+            opacity = self.transitions.get(
+                self.cb_transition.currentText()
+            ).__call__()
+
             yield opacity
-        
+
         return 0
 
-    def _toggle_start_process(self):
-        self.flag = not self.flag
-        self.start_btn.setText('Stop woggling' if self.flag else 'Start woggling')
-        if self.flag:
-            self._trigger_threaded_process()
-    
-    def _trigger_threaded_process(self):
-        self.layer = self.viewer.layers[self.cb_image.currentText()]
-        self.layer.opacity = 0.5
+    def _start(self):
+        self.is_woggling = not self.is_woggling
 
-        worker = self._threaded_process()
-        worker.yielded.connect(self._update_opacity)
-        worker.returned.connect(self._set_opacity_max)
+        self.start_btn.setText('Stop' if self.is_woggling else 'Start')
+        if self.is_woggling is False:
+            return
+        
+        self.layer = self.viewer.layers[self.cb_image.currentText()]
+
+        worker = self._threaded_woggling()
+        worker.yielded.connect(self._set_opacity)
         worker.start()
 
-    def _set_opacity_max(self, return_value):
-        self.layer.opacity = 1.0
-        self.layer.refresh()
-
-    def _update_opacity(self, opacity):
+    def _set_opacity(self, opacity):
         self.layer.opacity = opacity
-    
-    def _add_rename_event(self, e):
-        source_layer = e.value
-        source_layer.events.name.connect(self._proxy_on_layer_change)
-
-    def _proxy_on_layer_change(self, e):
-        self._on_layer_change(None)
-
-    def _on_layer_change(self, e):
-        self.cb_image.clear()
-        for x in self.viewer.layers:
-            self.cb_image.addItem(x.name, x.data)
